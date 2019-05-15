6a14be85a6c2afb2aac6b0f2bf528e87e9c3c414
Maruan Al-Shedivat <alshedivat.maruan@gmail.com>
2019-05-15 13:45:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,22 @@ from setuptools import setup, find_packages
 def read(filename):
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
+
 setup(
-    name='noisy-ml',
-    version='0.1dev',
-    license='Apache License 2.0',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    description='Learning from Noisy Labels',
-    url='https://github.com/eaplatanios/noisy-labels',
+    name="noisy-ml",
+    version="0.1dev",
+    license="Apache License 2.0",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    description="Learning from Noisy Labels",
+    url="https://github.com/eaplatanios/noisy-labels",
     install_requires=[
-        'nltk', 'numpy>=1.5', 'scikit-learn>=0.20.1', 'six',
-        'tensorflow>=1.11'])
+        "click",
+        "nltk",
+        "numpy>=1.5",
+        "scikit-learn>=0.20.1",
+        "six",
+        "tensorflow>=1.11",
+        "tqdm",
+    ],
+)

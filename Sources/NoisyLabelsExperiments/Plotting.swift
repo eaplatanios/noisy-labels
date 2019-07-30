b80e3272a9328d2c58e8119ed4e254b5c1d95c49
Anthony Platanios <e.a.platanios@gmail.com>
2019-07-30 14:15:15 -0400

    Made several improvements to the plotting script.

--- Sources/NoisyLabelsExperiments/Plotting.swift
+++ Sources/NoisyLabelsExperiments/Plotting.swift
@@ -18,6 +18,11 @@ import Python
 
 fileprivate let mpl = Python.import("matplotlib")
 fileprivate let plt = Python.import("matplotlib.pyplot")
+fileprivate let sns = Python.import("seaborn")
+
+fileprivate let redPalette = sns.color_palette("Reds", 4)
+fileprivate let greenPalette = sns.color_palette("Greens", 4)
+fileprivate let bluePalette = sns.color_palette("Blues_d", 4)
 
 public struct ResultsPlotter {
   public let resultsFile: URL
@@ -63,8 +68,13 @@ public struct ResultsPlotter {
       parsed[t]![m]![l]![p]!.append(v)
     }
 
-    mpl.use("TkAgg")
-    mpl.rc("text", usetex: false)
+    mpl.rcParams["pdf.fonttype"] = 42
+    mpl.rcParams["ps.fonttype"] = 42
+
+    sns.set()
+    sns.set_context("paper")
+    sns.set_style("white")
+    sns.set_style("ticks")
 
     let plotsFolder = resultsFile
       .deletingLastPathComponent()
@@ -78,24 +88,119 @@ public struct ResultsPlotter {
     // Plot all the results and save them in PDF files alongside the original results file.
     for (type, typeResults) in parsed {
       for (metric, metricResults) in typeResults {
-        let figure = plt.subplots()
-        let ax = figure[1]
-        for (learner, results) in metricResults {
+        // Create a new figure.
+        let figure = plt.figure(figsize: [5.0, 4.0])
+        let ax = plt.gca()
+
+        // Plot the curves for all the learners.
+        for (learner, results) in metricResults.sorted(by: { compareLearners($0.key, $1.key) }) {
           let results = results.sorted(by: { $0.0 < $1.0 })
           let x = results.map { $0.0 }
           let y = results.map { $0.1 }
           let yMean = y.map { $0.mean }
           let yStandardDeviation = y.map { $0.standardDeviation }
-          ax.plot(x, yMean, label: learner)
+          let color = learnerColor(learner)
+          ax.plot(x, yMean, label: learner, color: color, linewidth: 2)
           ax.fill_between(
             x,
             zip(yMean, yStandardDeviation).map(-),
             zip(yMean, yStandardDeviation).map(+),
-            alpha: 0.35)
+            color: color,
+            alpha: 0.1,
+            linewidth: 0)
+          ax.axhline(y: yMean.max(), linestyle: "dashed", alpha: 0.7, color: color)
         }
+
+        // Add axis labels.
+        ax.set_xlabel(
+          type.axisTitle,
+          color: "grey",
+          fontname: "Lato",
+          fontsize: 18,
+          fontweight: "light")
+        ax.set_ylabel(
+          metric.axisTitle,
+          color: "grey",
+          fontname: "Lato",
+          fontsize: 18,
+          fontweight: "light")
+        ax.yaxis.set_tick_params(labelbottom: true)
+
+        // Change the tick label sizes.
+        plt.setp(ax.get_xticklabels(), fontname: "Lato", fontsize: 18, fontweight: "regular")
+        plt.setp(ax.get_yticklabels(), fontname: "Lato", fontsize: 18, fontweight: "regular")
+
+        // Set the figure title.
+        figure.suptitle(
+          figureTitle(for: dataset),
+          x: 0.5,
+          y: 1,
+          fontname: "Lato",
+          fontsize: 22,
+          fontweight: "black")
+
+        // Remove the grid.
+        sns.despine()
+
+        // Add a legend.
         plt.legend()
-        plt.savefig(plotsFolder.appendingPathComponent("\(dataset)-\(type)-\(metric).pdf").path)
+
+        // Save the figure.
+        plt.savefig(
+          plotsFolder.appendingPathComponent("\(dataset)-\(type)-\(metric).pdf").path,
+          bbox_inches: "tight")
       }
     }
   }
+}
+
+extension ExperimentResult.ParameterType {
+  fileprivate var axisTitle: String {
+    switch self {
+    case .predictorCount: return "#Predictors"
+    case .redundancy: return "Redundancy"
+    }
+  }
+}
+
+extension Metric {
+  fileprivate var axisTitle: String {
+    switch self {
+    case .madError: return "Error MAD"
+    case .madErrorRank: return "Error Rank MAD"
+    case .accuracy: return "Accuracy"
+    case .auc: return "AUC"
+    }
+  }
+}
+
+fileprivate func figureTitle(for dataset: String) -> String {
+  switch dataset {
+  case "rte": return "RTE"
+  case _: return "Unknown"
+  }
+}
+
+fileprivate func compareLearners(_ learner1: String, _ learner2: String) -> Bool {
+  func score(_ learner: String) -> Int {
+    if learner.hasPrefix("LNL-F") { return 5 }
+    if learner.hasPrefix("LNL") { return 4 }
+    if learner.hasPrefix("Snorkel") { return 3 }
+    if learner.hasPrefix("MMCE-M") { return 2 }
+    if learner.hasPrefix("MAJ-S") { return 1 }
+    if learner.hasPrefix("MAJ") { return 0 }
+    return -1
+  }
+
+  return score(learner1) < score(learner2)
+}
+
+fileprivate func learnerColor(_ learner: String) -> PythonObject {
+  if learner.hasPrefix("LNL-F") { return redPalette[2] }
+    if learner.hasPrefix("LNL") { return redPalette[0] }
+    if learner.hasPrefix("Snorkel") { return greenPalette[2] }
+    if learner.hasPrefix("MMCE-M") { return bluePalette[1] }
+    if learner.hasPrefix("MAJ-S") { return bluePalette[2] }
+    if learner.hasPrefix("MAJ") { return bluePalette[3] }
+    return "black"
 }

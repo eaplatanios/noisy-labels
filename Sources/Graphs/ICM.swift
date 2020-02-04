abf78413d17f5cb19df2bbbdaf3fc78b352991ca
Emmanouil Antonios Platanios <e.a.platanios@gmail.com>
2020-02-04 13:40:34 -0500

    Added missing file from the previous commit.

---
+++ Sources/Graphs/ICM.swift
@@ -0,0 +1,91 @@
+
+func iteratedConditionalModes(
+  labelLogits: LabelLogits,
+  qualityLogits: [QualityLogits],
+  graph: Graph,
+  maxStepCount: Int = 100
+) -> [Int32] {
+  // logger.info("Starting ICM approximation of the labels mode.")
+  var labels = labelPropagation(graph: graph).map { $0.indexOfMax! }
+  let leveledData = graph.leveledData.suffix(from: 1)
+  for _ in 0..<maxStepCount {
+    var converged = true
+    for level in leveledData {
+      for node in level {
+        var scores = labelLogits.labelLogits(forNode: Int(node))
+        for (index, neighbor) in graph.neighbors[Int(node)].enumerated() {
+          for k in 0..<graph.classCount {
+            scores[k] += qualityLogits[Int(node)].qualityLogits(
+              node: index,
+              l: k,
+              k: labels[Int(neighbor)])
+          }
+        }
+        let label = scores.indexOfMax!
+        if labels[Int(node)] != label { converged = false }
+        labels[Int(node)] = label
+      }
+    }
+    if converged {
+      // logger.info("ICM converged in \(step) steps.")
+      break
+    }
+  }
+  return labels.map(Int32.init)
+}
+
+func labelPropagation(graph: Graph) -> [[Float]] {
+  var labelScores = [[Float]](
+    repeating: [Float](repeating: 0, count: graph.classCount),
+    count: graph.nodeCount)
+  var labeledNodes = Set<Int32>()
+  var nodesToLabel = Set<Int32>()
+
+  // Start with the labeled nodes.
+  for node in graph.trainNodes {
+    let label = graph.labels[node]!
+    labelScores[Int(node)][label] += 1
+    labeledNodes.update(with: node)
+    nodesToLabel.remove(node)
+    graph.neighbors[Int(node)].forEach {
+      if !labeledNodes.contains($0) {
+        nodesToLabel.update(with: $0)
+      }
+    }
+  }
+
+  // Proceed with label propagation for the unlabeled nodes.
+  while !nodesToLabel.isEmpty {
+    for node in nodesToLabel {
+      let labeledNeighbors = graph.neighbors[Int(node)].filter(labeledNodes.contains)
+      var scoresSum: Float = 0
+      for neighbor in labeledNeighbors {
+        for label in 0..<graph.classCount {
+          let score = labelScores[Int(neighbor)][label]
+          labelScores[Int(node)][label] += score
+          scoresSum += score
+        }
+      }
+      for label in 0..<graph.classCount {
+        labelScores[Int(node)][label] /= scoresSum
+      }
+    }
+    for node in nodesToLabel {
+      labeledNodes.update(with: node)
+      nodesToLabel.remove(node)
+      graph.neighbors[Int(node)].forEach {
+        if !labeledNodes.contains($0) {
+          nodesToLabel.update(with: $0)
+        }
+      }
+    }
+  }
+
+  for node in (0..<Int32(graph.nodeCount)).filter({ !labeledNodes.contains($0) }) {
+    for label in 0..<graph.classCount {
+      labelScores[Int(node)][label] = 1 / Float(graph.classCount)
+    }
+  }
+
+  return labelScores
+}
